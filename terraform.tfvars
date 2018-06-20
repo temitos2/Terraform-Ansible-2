71181a93fd4ce9f1d4af969085435daa03be276b
temitos2 <tfabiyi82@yahoo.com>
2018-06-20 18:11:08 +0000

    second commit

---
+++ terraform.tfvars
@@ -0,0 +1,14 @@
+// access options
+aws_access_key 	= "AKIAIUDRJRQ5MLVEXFTA"
+aws_secret_key 	= "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"
+aws_key_name   	= "codedeploy"
+
+// instance options
+aws_region     		= "us-east-1"
+ami 				= "ami-a4dc46db"
+instance_type   	= "t2.micro"
+
+// networking options
+vpc_cidr 			= "10.0.0.0/16"
+public_subnet_cidr 	= "10.0.0.0/24"
+private_subnet_cidr = "10.0.1.0/24"
---
+++ vpc/variables.tf
@@ -0,0 +1,33 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_key_name" {}
+
+variable "aws_region" {
+  description = "EC2 Region for the VPC"
+  default     = "us-east-1"
+}
+
+variable "ami" {
+  description = "AMI id"
+  default     = "ami-a4dc46db" # Ubuntu
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  default = "t2.micro"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for the whole VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for the Public Subnet"
+  default     = "10.0.0.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR for the Private Subnet"
+  default     = "10.0.1.0/24"
+}
